0e6071cabab13ec847640069212f01ea8823c81c
Hareesh Gottipati <hg.datahive@gmail.com>
2025-09-18 11:25:52 -0700

    Initial Commit

---
+++ StrongMe/StrongMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrongMeApp.swift
+//  StrongMe
+//
+//  Created by Hareesh Gottipati on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct StrongMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
